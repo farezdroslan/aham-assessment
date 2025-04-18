84ac89dc952ce95396991635e8f037f4b9f2ebfe
farezdroslan <farezdzuanbinroslan@gmail.com>
2025-04-18 10:04:12 +0800

    migrate and view update

--- aham-fund-management-system/fund/settings.py
+++ aham-fund-management-system/fund/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,10 +81,21 @@ WSGI_APPLICATION = 'fund.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fundapp',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- aham-fund-management-system/fundapp/views.py
+++ aham-fund-management-system/fundapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Fund
+from .models import Fund, Manager
 from .serializer import FundSerializer
 
 # Create your views here.
@@ -33,6 +33,11 @@ def create_fund(request):
     Create a new fund and return it as JSON.
     """
     if request.method == 'POST':
+         # Validate that the manager_id exists in the database
+        manager_id = request.data.get('fund_manager')
+        if not Manager.objects.filter(manager_id=manager_id).exists():
+            return Response({"error": "Invalid manager_id provided."}, status=status.HTTP_400_BAD_REQUEST)
+        
         # Deserialize the incoming data
         serializer = FundSerializer(data=request.data)
         
@@ -50,20 +55,27 @@ def fund_detail(request, pk):
     try:
         fund = Fund.objects.get(pk=pk)
     except Fund.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response({"detail": "Not found"},status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({"error": "An unexpected error occured."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
     
     if request.method == 'GET':
         serializer = FundSerializer(fund)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     elif request.method == 'PUT':
+        manager_id = request.data.get('fund_manager')
+        if manager_id and not Manager.objects.filter(manager_id=manager_id).exists():
+            return Response({"error": "Invalid manager_id provided."}, status=status.HTTP_400_BAD_REQUEST)
+        
         serializer = FundSerializer(fund, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     if request.method == 'DELETE':
         fund.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": "Fund deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
 
